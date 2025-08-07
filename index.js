8743720f529a6f5303a5951544c1838c203a43a4
sushanthumiitech <sushantsrk2001@gmail.com>
2025-08-07 07:15:00 +0530

    added the otp schema

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 dotenv.config();
 import User from "./schemas/user.schema.js";
 import Kyc from "./schemas/kyc.schema.js";
-
+import Otp from "./schemas/otp.schema.js";
 import connectDB from "./db.js";
 
 connectDB();
-export { connectDB, User, Kyc };
+export { connectDB, User, Kyc, Otp };
---
+++ schemas/otp.schema.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const otpSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    otp: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      expires: "1m",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Otp = mongoose.model("Otp", otpSchema);
+
+export default Otp;
