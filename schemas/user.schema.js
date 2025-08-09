18c31176971c0f89c99f5144d6bcdb5f1ab1c86c
sushanthumiitech <sushantsrk2001@gmail.com>
2025-08-09 07:31:03 +0530

    added the is active field in user schema

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -69,6 +69,10 @@ const userSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     timestamps: true,
