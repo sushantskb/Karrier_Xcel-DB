cb98710d6b2b414331a2c48df1ae6269b252ce39
sushanthumiitech <sushantsrk2001@gmail.com>
2025-08-10 09:20:23 +0530

    updated the dev dependency

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import User from "./schemas/user.schema.js";
 import Kyc from "./schemas/kyc.schema.js";
 import Otp from "./schemas/otp.schema.js";
 import Package from "./schemas/package.schema.js";
+import Request from "./schemas/request.schema.js";
 import connectDB from "./db.js";
 
 connectDB();
-export { connectDB, User, Kyc, Otp, Package };
+export { connectDB, User, Kyc, Otp, Package, Request };
