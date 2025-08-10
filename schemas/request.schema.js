b5eee619fc625ed16643bc48368607c4d7f8918f
sushanthumiitech <sushantsrk2001@gmail.com>
2025-08-10 09:14:36 +0530

    added the request model

---
+++ schemas/request.schema.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const requestSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    package: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Package",
+    },
+    status: {
+      type: String,
+      enum: ["pending", "approved", "rejected"],
+      default: "pending",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Request = mongoose.model("Request", requestSchema);
+export default Request;
