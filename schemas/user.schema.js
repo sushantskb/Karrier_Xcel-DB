1f3661c4597a9958d00f4439a9dbb0aaa9bfcf33
Sushant Kumar Bishoi <91365425+sushantskb@users.noreply.github.com>
2025-09-15 17:38:54 +0530

    Update user.schema.js

--- schemas/user.schema.js
+++ schemas/user.schema.js
@@ -102,10 +102,6 @@ const userSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
-    isPaid: {
-      type: Boolean,
-      default: false,
-    },
     isBlocked: {
       type: Boolean,
       default: false,
